290178f0427667653ab231a8bec22572d3d6fb42
acapobia23 <acapobia58@gmail.com>
2025-07-23 02:32:12 +0200

    update

--- docs/js/nightlife/weekly-tips.js
+++ docs/js/nightlife/weekly-tips.js
@@ -74,7 +74,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const sendMsg = method => {
     const val = id => document.getElementById(id)?.value.trim() || '';
     const lines = [
-      `Hello! I'd like to book "weekly tips".`,
+      `Hello! I'd like to book "Weekly Tips".`,
       ``,
       `👤 Name:  ${val("main-guest")}`,
       `📧 Email: ${val("email")}`,
