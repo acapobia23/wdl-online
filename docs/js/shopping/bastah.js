ad4580b59c5ce0e760fa1781a11710f398bd6633
acapobia23 <acapobia58@gmail.com>
2025-08-22 21:44:54 +0200

    update

--- docs/js/shopping/bastah.js
+++ docs/js/shopping/bastah.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // === GALLERY ===
   const galleryContainer = document.getElementById("gallery-container");
   if (galleryContainer) {
-    const imageFiles = ["00.png","01.jpg","02.jpg","03.jpg","04.jpg","05.jpg","06.jpg","07.jpg","08.jpg","09.jpg"]; //file name of pic
+    const imageFiles = ["00.png","01.jpg","02.jpg","03.jpg","04.jpg","05.jpg","06.jpg","08.jpg","09.jpg"]; //file name of pic
     const basePath = "../../assets/img/boxes/shopping/bastah/"; //path pic
     const images = imageFiles.map(f => basePath + f);
 //cambiare alt name linea 14
