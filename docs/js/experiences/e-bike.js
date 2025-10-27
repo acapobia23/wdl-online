4d56127c630b1843d5d0d1513229f775460e2cae
acapobia23 <acapobia58@gmail.com>
2025-10-27 21:35:24 +0100

    weekly tips e ebike

--- docs/js/experiences/e-bike.js
+++ docs/js/experiences/e-bike.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // === GALLERY ===
   const galleryContainer = document.getElementById("gallery-container");
   if (galleryContainer) {
-    const imageFiles = ["01.jpg","02.jpg","03.png","04.jpg"]; //file name of pic
+    const imageFiles = ["01.jpg","02.jpg","03.webp","04.jpg"]; //file name of pic
     const basePath = "../../assets/img/boxes/experience/e-bike/"; //path pic
     const images = imageFiles.map(f => basePath + f);
 //cambiare alt name linea 14
@@ -55,7 +55,7 @@ document.addEventListener("DOMContentLoaded", () => {
         <label class="bold-text" for="date-picker">Add info and chat!</label>
         <div><p></p></div><p class="bold-gray">*mandatory field</p> 
       <input type="text" id="main-guest" placeholder="*Name and Surname" required>
-
+      <input type="text" id="host" placeholder="*Who did you book your stay with?" required>
         <input type="text" id="date-picker" placeholder="Select a date" readonly>
         <select id="guest-picker">
           ${[...Array(6)].map((_,i)=>
@@ -95,11 +95,11 @@ document.addEventListener("DOMContentLoaded", () => {
     });
     // "Live the Dolce Vita in Florence - eFiat 500 Tour"
     const lines = [
-      `Hello! I'm staying at Velona's Jungle and I'd like to book this ${experience}.`,
+      `Hello! I'd like to book ${experience}.`,
       ``,
       `📅 Date:  ${val("date-picker")}`,
     `👤 Name:  ${val("main-guest")}`,
-
+    `🏠 Host:  ${val("host")}`,
       `🧑‍🤝‍🧑 Adults: ${val("guest-picker")}`,
       `👶 Minors: ${val("under-18")}`,
       `📧 Email: ${val("email")}`,
