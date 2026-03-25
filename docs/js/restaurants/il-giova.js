28d82a8e6fc090ea2da0130ab7de78e81896c3e4
acapobia23 <acapobia58@gmail.com>
2026-03-25 19:44:32 +0100

    giova

--- docs/js/restaurants/il-giova.js
+++ docs/js/restaurants/il-giova.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // === GALLERY ===
   const galleryContainer = document.getElementById("gallery-container");
   if (galleryContainer) {
-    const imageFiles = ["01.jpg","02.jpg","03.jpg","04.jpg"]; //file name of pic
+    const imageFiles = ["01.jpg","02.jpg","03.jpg"]; //file name of pic
     const basePath = "../../assets/img/boxes/restaurants/il-giova/"; //path pic
     const images = imageFiles.map(f => basePath + f);
 //cambiare alt name linea 14
